7139bf5dfd738b31a330b2be5dc6d9a2e6495e79
lomeat <lom3at@gmail.com>
2020-08-06 19:06:02 +0300

    add: methods (find, remove) to manage item in tree

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
+type Item = {
+  id: number;
+  title: string;
+  children?: Item[];
+};
+
 export const Sidebar = () => {
   const [tree, setTree] = useState([
     {
@@ -29,6 +35,54 @@ export const Sidebar = () => {
       ],
     },
   ]);
+
+  const moveItem = (id: number, afterId: number, nodeId: number) => {
+    if (id === afterId) return;
+
+    const removeNode = (id: number, items: Item[]) => {
+      for (const node of items) {
+        if (node.id === id) {
+          items.splice(items.indexOf(node), 1);
+          return;
+        }
+
+        if (node.children && node.children.length) {
+          removeNode(id, node.children);
+        }
+      }
+    };
+
+    const item: Item = { ...findItem(id, tree) };
+    if (isNaN(item.id)) {
+      return;
+    }
+
+    const dest: any = nodeId ? findItem(nodeId, tree).children : tree;
+
+    if (!afterId) {
+      removeNode(id, tree);
+      dest.push(item);
+    } else {
+      const index = dest.indexOf(
+        dest.filter((a: Item) => a.id === afterId).shift()
+      );
+      removeNode(id, tree);
+      dest.splice(index, 0, item);
+    }
+  };
+
+  const findItem = (id: number, items: Item[]): Item => {
+    for (const node of items) {
+      if (node.id === id) return node;
+      if (node.children && node.children.length) {
+        const result: Item = findItem(id, node.children);
+        if (result) {
+          return result;
+        }
+      }
+    }
+    return { id: NaN, title: "Undefined" };
+  };
 
   return (
     <Wrapper>
